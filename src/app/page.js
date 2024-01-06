5c71ada6bdd65670e9fc71b4f9909c6d4f504e30
Josh Michael Olea <34050740+joshmichael23@users.noreply.github.com>
2024-01-06 16:49:59 +0800

    2024-01-06 first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,148 @@
+"use client"
 import Image from 'next/image'
-import styles from './page.module.css'
+import style from './page.css'
+import moment from 'moment';
+
+import { useState, useRef, useEffect } from 'react';
 
 export default function Home() {
+  // The state for our timer
+
+  function computeDays(time){
+    return Math.floor(time / (3600 * 24));
+  }
+
+  function computeHours(time){
+    return Math.floor((time % (60 * 60 * 24)) / (3600));
+  }
+
+  function computeMinutes(time){
+    return Math.floor((time % (60 * 60)) / 60);
+  }
+
+  function computeSeconds(time){
+    return Math.floor(time % 60);
+  }
+
+  function zeroPad(num) {
+    
+      return num.toString().padStart(2, "0");
+    
+  }
+
+  const [time, setTime] = useState(777600);
+  const [days, setDay] = useState(computeDays(time));
+  const [hours, setHour] = useState(computeHours(time));
+  const [minutes, setMinutes] = useState(computeMinutes(time));
+  const [seconds, setSeconds] = useState(computeSeconds(time));
+
+  const prevCountDay = useRef();
+  const prevCountHour = useRef();
+  const prevCountMin = useRef();
+  const prevCountRef = useRef();
+
+  useEffect(() => {
+    if (time > 0) {
+
+      const check = days;
+      const timer = setInterval(() => {
+        setTime(prevTime => prevTime - 1);
+        setDay(prevDay => computeDays(time));
+        setHour(prevHour => computeHours(time));
+        setMinutes(prevMinutes => computeMinutes(time));
+        setSeconds(prevSeconds => computeSeconds(time));
+
+        prevCountDay.current = zeroPad(days);
+        prevCountHour.current = zeroPad(hours);
+        prevCountMin.current = zeroPad(minutes);
+        prevCountRef.current = zeroPad(seconds);
+      }, 1000);
+
+      
+
+      return () => clearInterval(timer);
+    }
+  }, [time])
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <main>
+          <div className="wrapper">
+            <h1>We're launching soon</h1> 
+            <div className="timer">
+              <div className="days">
+                  <div className="card">
+                    
+                    <div className="card-upper">
+                      <p>{zeroPad(days)}</p>
+                    </div>
+                    <div className="card-lower">
+                        <div className={prevCountDay.current != days ? "card-lower-inner fold" : "card-lower-inner"}>
+                          <div className="card-lower-front"><p>{prevCountDay.current}</p></div>
+                          <div className="card-lower-back"></div>
+                          
+                        </div>
+                        <p>{zeroPad(days)}</p>
+                    </div>
+                  </div>
+                  <p>Days</p>
+              </div>
+              <div className="hours">
+              <div className="card">
+                    
+                    <div className="card-upper">
+                      <p>{zeroPad(hours)}</p>
+                    </div>
+                    <div className="card-lower">
+                        <div className={prevCountHour.current != hours ? "card-lower-inner fold" : "card-lower-inner"}>
+                          <div className="card-lower-front"><p>{prevCountHour.current}</p></div>
+                          <div className="card-lower-back"></div>
+                        </div>
+                        <p>{zeroPad(hours)}</p>
+                    </div>
+                  </div>
+                  <p>Hours</p>
+              </div>
+              <div className="minutes">
+              <div className="card">
+                    
+                    <div className="card-upper">
+                      <p>{zeroPad(minutes)}</p>
+                    </div>
+                    <div className="card-lower">
+                        <div className={prevCountMin.current != minutes ? "card-lower-inner fold" : "card-lower-inner"}>
+                          <div className="card-lower-front"><p>{prevCountMin.current}</p></div>
+                          <div className="card-lower-back"></div>
+                        </div>
+                        <p>{zeroPad(minutes)}</p>
+                    </div>
+                  </div>
+                  <p>Minutes</p>
+              </div>
+              <div className="seconds">
+              <div className="card">
+                    <h1></h1>
+                    <div className="card-upper">
+                      <p>{zeroPad(seconds)}</p>
+                    </div>
+                    <div className="card-lower">
+                        <div className={prevCountRef.current != seconds ? "card-lower-inner fold" : "card-lower-inner"}>
+                          <div className="card-lower-front"><p>{prevCountRef.current}</p></div>
+                          <div className="card-lower-back"></div>
+                          
+                        </div>
+                        <p>{zeroPad(seconds)}</p>
+                    </div>
+                  </div>
+                  <p>Seconds</p>
+              </div>
+            </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          </div>
+          <footer className='social-media'> 
+            <a className="facebook" href=""></a>
+            <a className="pinterest" href=""></a>
+            <a className="instagram" href=""></a>
+        </footer>
+      </main>
   )
 }
